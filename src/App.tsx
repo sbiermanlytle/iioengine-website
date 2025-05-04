ca2772d5a18ac6ca52d56eea7fc44a69b7295cbc
Sebastian Bierman-Lytle <sebastianbierman-lytle@Sebastians-MacBook-Pro.local>
2025-05-03 18:38:28 -0700

    install MUI and env file

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import iio from 'iioengine'
 import './App.css'
 
 function App() {
+  const value = import.meta.env.VITE_VISIBLE_VAR
   const [count, setCount] = useState(0)
+  iio.printMsg()
 
   return (
     <>
@@ -16,7 +19,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Vite + React + {value}</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
